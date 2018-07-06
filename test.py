dcf06daecee382f8284b05b00f31efb315d8e254
varaprasadtdp <varaprasadtdp@gmail.com>
2018-07-06 15:48:31 +0530

    Update test.py
    new commit

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python
 print ("Welcome To Hello World")
 print ("Welcome To master branch")
-<<<<<<< HEAD
-# python code added .//
-=======
-# python code added 
-# Hello
->>>>>>> 4a6730302cddb0439b5ee4ea55a98fb601a2ed1a
